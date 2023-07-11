f5ccf552a6f0211c38a33d30841cfc891f44c3dd
saniul06 <saniul428@gmail.com>
2023-07-11 20:11:07 +0600

    Add register functionality

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,19 +1,36 @@
+import cloudinary from 'cloudinary'
 import User from '../models/user'
 
 import catchAsyncErrors from '../middlewares/catchAsyncErrors'
 
+// Setting up cloudinary config
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+})
+
+
 // Register user   =>   /api/auth/register
 const registerUser = catchAsyncErrors(async (req, res) => {
 
-    const { name, email, password, role } = req.body;
+    const { name, email, password, role, avatar } = req.body;
+
+    console.log('req.body: ', req.body)
+
+    const result = await cloudinary.v2.uploader.upload(avatar, {
+        folder: 'bookit/avatars',
+        width: '150',
+        crop: 'scale'
+    })
 
     const user = await User.create({
         name,
         email,
         password,
         avatar: {
-            public_id: 'pulicid',
-            url: 'url'
+            public_id: result.public_id,
+            url: result.secure_url
         },
         role
     });
--- redux/reducers/reducers.js
+++ redux/reducers/reducers.js
@@ -1,9 +1,11 @@
 import { combineReducers } from "redux";
 import { allRoomsReducer, roomDetailsReducer } from "./roomReducer";
+import { authReducer } from "./userReducers";
 
 const reducers = combineReducers({
     allRooms: allRoomsReducer,
     roomDetails: roomDetailsReducer,
+    auth: authReducer,
 });
 
 export default reducers;
